9df00eb8004c85f143e55d81ca0e4833769ff067
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-01-19 22:53:09 +0530

    Add project files.

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlackCoderBankTransactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {}
+            
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
